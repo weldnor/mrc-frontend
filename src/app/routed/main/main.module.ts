ca267e75b27bab0b7ff85f2b85920e3b54166871
weldnor <kolesov.anton.s@gmail.com>
2021-05-06 15:12:52 +0300

    Create simple login form

--- src/app/routed/main/main.module.ts
+++ src/app/routed/main/main.module.ts
@@ -2,7 +2,11 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HomePage} from './pages/home/home.page';
 import {MainRoutingModule} from './main-routing.module';
-import { LoginPage } from './pages/login/login.page';
+import {LoginPage} from './pages/login/login.page';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import {MatOptionModule} from '@angular/material/core';
+import {MatInputModule} from '@angular/material/input';
 
 
 @NgModule({
@@ -13,6 +17,10 @@ import { LoginPage } from './pages/login/login.page';
   imports: [
     CommonModule,
     MainRoutingModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatInputModule,
+    MatOptionModule,
   ]
 })
 export class MainModule {
