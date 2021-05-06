fc75ba28cef8a8b4097068c239dbcd91c4f25c15
weldnor <kolesov.anton.s@gmail.com>
2021-05-06 13:47:26 +0300

    Add login page, update routing

--- src/app/routed/main/main-routing.module.ts
+++ src/app/routed/main/main-routing.module.ts
@@ -1,10 +1,12 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomePage} from './pages/home/home.page';
+import {LoginPage} from './pages/login/login.page';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'prefix'},
   {path: 'home', component: HomePage},
+  {path: 'login', component: LoginPage},
 ];
 
 @NgModule({
--- src/app/routed/main/main.module.ts
+++ src/app/routed/main/main.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HomePage} from './pages/home/home.page';
 import {MainRoutingModule} from './main-routing.module';
+import { LoginPage } from './pages/login/login.page';
 
 
 @NgModule({
   declarations: [
     HomePage,
+    LoginPage,
   ],
   imports: [
     CommonModule,
