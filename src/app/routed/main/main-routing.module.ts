4c2e79a06ed78ec4be4c1f6e96603e00d9275dd0
weldnor <kolesov.anton.s@gmail.com>
2021-05-08 02:27:09 +0300

    Add register page

--- src/app/routed/main/main-routing.module.ts
+++ src/app/routed/main/main-routing.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomePage} from './pages/home/home.page';
 import {LoginPage} from './pages/login/login.page';
+import {RegisterPage} from './pages/register/register.page';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'prefix'},
   {path: 'home', component: HomePage},
   {path: 'login', component: LoginPage},
+  {path: 'register', component: RegisterPage},
 ];
 
 @NgModule({
