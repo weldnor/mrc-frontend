c0a9d418e030f2abf7984221be9fc5617918a88a
weldnor <kolesov.anton.s@gmail.com>
2021-05-13 23:43:18 +0300

    Fix server static paths

--- server.js
+++ server.js
@@ -22,10 +22,10 @@ const forceSSL = function () {
 
 app.use(forceSSL());
 
-app.use(express.static('./dist/my-app111'));
+app.use(express.static('./dist/mrc-frontend'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/my-app111/'}),
+  res.sendFile('index.html', {root: 'dist/mrc-frontend/'}),
 );
 
 app.listen(process.env.PORT || 8080);
